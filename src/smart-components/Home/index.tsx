deb4b62918069ba0d1fc6adcc12e81b34cd0f34b
HunteRoi <me@tinaeldevresse.eu>
2023-02-18 22:27:14 +0100

    feat: font edition

--- src/smart-components/Home/index.tsx
+++ src/smart-components/Home/index.tsx
@@ -3,16 +3,13 @@ import { SettingOutlined } from '@ant-design/icons';
 
 import SicknessCard from '../SicknessCard';
 import Settings from '../Settings';
-import { Modal } from 'antd';
+import { Button, Modal } from 'antd';
 
 const Home: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const showModal = () => setIsModalOpen(true);
     const handleOk = () => {
-        setIsModalOpen(false);
-    };
-    const handleCancel = () => {
         setIsModalOpen(false);
     };
 
@@ -22,7 +19,14 @@ const Home: React.FC = () => {
         <SicknessCard name='Dyslexia' />
         <SettingOutlined onClick={showModal}/>
 
-        <Modal title='Paramètres' open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Modal
+            title='Paramètres'
+            open={isModalOpen}
+            onOk={handleOk}
+            footer={[
+                <Button type='primary' onClick={handleOk}>OK</Button>
+            ]}
+        >
             <Settings />
         </Modal>
     </>;
