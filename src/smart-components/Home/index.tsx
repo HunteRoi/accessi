12bdd1e6352f5bb660a73246902f8170877a299e
isaline <isaline.didier@outlook.com>
2023-02-18 20:31:11 +0100

    home grid css

--- src/smart-components/Home/index.tsx
+++ src/smart-components/Home/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Button, Modal } from 'antd';
 import { SettingOutlined } from '@ant-design/icons';
 
+import './style.css';
 import SicknessCard from '../SicknessCard';
 import Settings from '../Settings';
 
@@ -14,9 +15,26 @@ const Home: React.FC = () => {
     };
 
     return <>
-        <SicknessCard name='TCA' />
-        <SicknessCard name='Schizophrenia' />
-        <SicknessCard name='Dyslexia' />
+        <div className="parent">
+            <div className="div1">Choisir une expérience</div>
+            <div className="div2">
+                <SicknessCard name='Dyslexie' />
+            </div>
+            <div className="div3">Truc 1</div>
+            <div className="div4">Schizo</div>
+            <div className="div5">Truc 2</div>
+            <div className="div6">Daltonisme</div>
+            <div className="div7">Surdité</div>
+            <div className="div8">Suggestions</div>
+            <div className="div9">
+                <SicknessCard name='TCA' />
+            </div>
+            <div className="div10">Dyscalculie</div>
+            <div className="div11">Malvoyant</div>
+            <div className="div12">Dysorthographie </div>
+        </div> 
+        
+
         <SettingOutlined onClick={showModal}/>
 
         <Modal
